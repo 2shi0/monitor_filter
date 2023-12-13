e91f56f238643ce627b843cd95039676eadf85d4
Tsushima <b1019217@fun.ac.jp>
2023-12-13 18:25:24 +0900

    クラスにした

--- tsukamoto.py
+++ tsukamoto.py
@@ -1,16 +1,31 @@
 from PySide6 import QtCore, QtWidgets
 import sys
 
-app = QtWidgets.QApplication(sys.argv)
 
-label = QtWidgets.QLabel()
-label.setAttribute(QtCore.Qt.WA_TransparentForMouseEvents)  # マウスイベント透過
-label.setWindowFlags(QtCore.Qt.FramelessWindowHint |  # 枠なしにする
-                     QtCore.Qt.WindowStaysOnTopHint)  # 常に最前面に表示
-label.setGeometry(0, 0, 1920, 1080)  # 全画面に
-label.setStyleSheet('background-color: #111111;')  # 背景色の定義
-label.setWindowOpacity(0.5)  # 透明度設定
+class qt_filter():
+    def __init__(self):
+        self.label = QtWidgets.QLabel()
+        self.label.setAttribute(
+            QtCore.Qt.WA_TransparentForMouseEvents)  # マウスイベント透過
+        self.label.setWindowFlags(QtCore.Qt.FramelessWindowHint |  # 枠なしにする
+                                  QtCore.Qt.WindowStaysOnTopHint)  # 常に最前面に表示
+        self.label.setGeometry(0, 0, 1920, 1080)  # 全画面に
+        self.label.setStyleSheet('background-color: #111111;')  # 背景色の定義
+        self.label.setWindowOpacity(0.5)  # 透明度設定
 
-label.show()
+        self.label.show()
 
-sys.exit(app.exec())
+    def update(self, n):
+        """
+        画面の暗さを変更する（nは0.0~1.0の値）
+        """
+        self.label.setWindowOpacity(n)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = qt_filter()
+
+    main_window.update(0.8)
+
+    sys.exit(app.exec())
